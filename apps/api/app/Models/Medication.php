92da644b6ec16fef7b2f40b42a08d80fcd695582
Henrique Pappis <contact@henriquepappis.com>
2025-12-26 11:55:32 -0300

    Fix PHPStan with Larastan and model annotations

--- apps/api/app/Models/Medication.php
+++ apps/api/app/Models/Medication.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @property int $user_id
+ */
 class Medication extends Model
 {
     protected $fillable = [
